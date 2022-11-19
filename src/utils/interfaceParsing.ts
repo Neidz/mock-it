789c60131a966b774bd48de6767c9b0a03ed8c94
Neidz <adamo7255@gmail.com>
2022-11-19 21:23:43 +0100

    created basic extraction of name and entries from interface using regex

--- src/utils/interfaceParsing.ts
+++ src/utils/interfaceParsing.ts
@@ -1,14 +1,54 @@
-interface MockedDataStructure {
+// interface TestInterface {
+//     name: string;
+//     values: number;
+//     type: string;
+//     testValue: number;
+// }
+
+interface MockedDataStructureType {
     name: string;
-    values: {
-        name: string;
+    entries: {
+        key: string;
         type: string;
     }[];
 }
 
-export const createMockFromInterface = (inputInterface: string) => {
-    const nameTest = /interface\s([a-zA-Z0-9]+)\s{.+/;
-    const name = inputInterface.match(nameTest)?.[1] ?? 'nameNotFound';
+const getInterfaceName = (testString: string): string => {
+    const interfaceNameRegex = /(?<=interface\s+)([a-zA-Z0-9]+)(?=\s+{)/;
 
-    return name;
+    return testString.match(interfaceNameRegex)?.[0] ?? 'nameNotFound';
+};
+
+const getKeys = (testString: string): string[] => {
+    const keysRegex = /(?<=\s)([a-zA-Z0-9]+)(?=\s{0,}:)/g;
+
+    return testString.match(keysRegex) ?? [];
+};
+
+const getTypes = (testString: string): string[] => {
+    const typesRegex = /(?<=:\s{0,})([a-zA-Z0-9]+)(?=[\s|;])/g;
+
+    return testString.match(typesRegex) ?? [];
+};
+
+const buildEntries = (
+    keys: string[],
+    types: string[]
+): { key: string; type: string }[] => {
+    return keys.map((key, index) => {
+        return { key, type: types[index] ?? 'wrongValue' };
+    });
+};
+
+export const createMockFromInterface = (inputInterface: string) => {
+    const mockedDataStructure: MockedDataStructureType = {
+        name: getInterfaceName(inputInterface),
+        entries: buildEntries(
+            getKeys(inputInterface),
+            getTypes(inputInterface)
+        ),
+    };
+    console.log(mockedDataStructure);
+
+    return inputInterface;
 };
